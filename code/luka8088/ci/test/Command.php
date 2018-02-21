1ee07e07f420a8821bb51452af16fddbecac7e81
luka8088 <luka8088@nrsk.net>
2018-02-21 23:12:23 +0000

    Alingnment

--- code/luka8088/ci/test/Command.php
+++ code/luka8088/ci/test/Command.php
@@ -14,52 +14,52 @@ use \Symfony\Component\Console\Output\OutputInterface;
 
 class Command extends \Symfony\Component\Console\Command\Command {
 
-    protected function configure () {
-      $this
-        ->setName('test')
-        ->setDescription('Run code analysis and testing tools.')
-        ->setHelp('Run code analysis and testing tools.')
-        ->addOption('tool', null, InputOption::VALUE_OPTIONAL, 'Run only a specific tool.')
-      ;
-      op\metaContext(ExtensionInterface::class)[] = [
-        /** @ExtensionCall('luka8088.ci.test.configureCommand') */ function ($command) {},
-      ];
-      op\metaContext(ExtensionInterface::class)['luka8088.ci.test.configureCommand']->__invoke($this);
+  function configure () {
+    $this
+      ->setName('test')
+      ->setDescription('Run code analysis and testing tools.')
+      ->setHelp('Run code analysis and testing tools.')
+      ->addOption('tool', null, InputOption::VALUE_OPTIONAL, 'Run only a specific tool.')
+    ;
+    op\metaContext(ExtensionInterface::class)[] = [
+      /** @ExtensionCall('luka8088.ci.test.configureCommand') */ function ($command) {},
+    ];
+    op\metaContext(ExtensionInterface::class)['luka8088.ci.test.configureCommand']->__invoke($this);
+  }
+
+  function execute (InputInterface $input, OutputInterface $output) {
+
+    $inputMetaContext = op\metaContextCreateScoped(InputInterface::class, $input);
+    $outputMetaContext = op\metaContextCreateScoped(OutputInterface::class, $output);
+    $resultMetaContext = op\metaContextCreateScoped(Result::class, new Result());
+
+    // @todo: Rething.
+    $hasConsole = false;
+    foreach (op\metaContext(Application::class)->extensions as $extension)
+      if ($extension instanceof \luka8088\ci\test\report\Console)
+        $hasConsole = true;
+    if (!$hasConsole)
+      op\metaContext(Application::class)[] = new \luka8088\ci\test\report\Console();
+
+    op\metaContext(ExtensionInterface::class)["luka8088.ci.test.begin"]->__invoke();
+    op\metaContext(ExtensionInterface::class)["luka8088.ci.test.run"]->__invoke();
+
+    $testers = [];
+    foreach (op\metaContext(Application::class)->extensions as $extension)
+      if (method_exists($extension, 'runTests'))
+        $testers[$extension->getIdentifier()] = $extension;
+
+    if ($input->getOption('tool', '') && !isset($testers[$input->getOption('tool', '')]))
+      throw new Exception('Tool not registered.');
+
+    foreach ($testers as $testerIdentifier => $tester) {
+      if ($input->getOption('tool', '') && $input->getOption('tool', '') != $testerIdentifier)
+        continue;
+      $tester->runTests();
     }
 
-    protected function execute (InputInterface $input, OutputInterface $output) {
+    op\metaContext(ExtensionInterface::class)["luka8088.ci.test.end"]->__invoke();
 
-      $inputMetaContext = op\metaContextCreateScoped(InputInterface::class, $input);
-      $outputMetaContext = op\metaContextCreateScoped(OutputInterface::class, $output);
-      $resultMetaContext = op\metaContextCreateScoped(Result::class, new Result());
-
-      // @todo: Rething.
-      $hasConsole = false;
-      foreach (op\metaContext(Application::class)->extensions as $extension)
-        if ($extension instanceof \luka8088\ci\test\report\Console)
-          $hasConsole = true;
-      if (!$hasConsole)
-        op\metaContext(Application::class)[] = new \luka8088\ci\test\report\Console();
-
-      op\metaContext(ExtensionInterface::class)["luka8088.ci.test.begin"]->__invoke();
-      op\metaContext(ExtensionInterface::class)["luka8088.ci.test.run"]->__invoke();
-
-      $testers = [];
-      foreach (op\metaContext(Application::class)->extensions as $extension)
-        if (method_exists($extension, 'runTests'))
-          $testers[$extension->getIdentifier()] = $extension;
-
-      if ($input->getOption('tool', '') && !isset($testers[$input->getOption('tool', '')]))
-        throw new Exception('Tool not registered.');
-
-      foreach ($testers as $testerIdentifier => $tester) {
-        if ($input->getOption('tool', '') && $input->getOption('tool', '') != $testerIdentifier)
-          continue;
-        $tester->runTests();
-      }
-
-      op\metaContext(ExtensionInterface::class)["luka8088.ci.test.end"]->__invoke();
-
-    }
+  }
 
 }
