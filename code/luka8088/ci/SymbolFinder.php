bee1d9df85d42cbba241367d75f29d1173723836
luka8088 <luka8088@nrsk.net>
2018-03-08 21:57:54 +0000

    SymbolFinder tweaks

--- code/luka8088/ci/SymbolFinder.php
+++ code/luka8088/ci/SymbolFinder.php
@@ -7,12 +7,13 @@ class SymbolFinder {
   protected $data = [];
 
   function findByLocation ($file, $line, $column, $fileSymbol) {
-    if (!isset($this->data[$file . ":maxColumn"])) {
+    if (!isset($this->data[$file])) {
+      $this->data[$file] = [];
       if (is_file($file)) {
         static $nameTokens = [T_WHITESPACE, T_STRING, T_NS_SEPARATOR];
         $contextStack = [
-          ["name" => "", "entered" => false, "isClass" => false],
-          ["name" => "", "entered" => false, "isClass" => false],
+          ['name' => '', 'entered' => false, 'isClass' => false],
+          ['name' => '', 'entered' => false, 'isClass' => false],
         ];
         $beginIndex = 0;
         $maxColumn = 1;
@@ -42,56 +43,53 @@ class SymbolFinder {
           if ($tokens[$index][0] == T_NAMESPACE) {
             if ($beginIndex == 0)
               $beginIndex = $index;
-            $name = "";
+            $name = '';
             $nextToken();
             while (isset($tokens[$index]) && in_array($tokens[$index][0], $nameTokens)) {
               $name .= trim($tokens[$index][1]);
               $nextToken();
             }
-            array_unshift($contextStack, ["name" => $name, "entered" => false, "isClass" => false]);
-            if ($contextStack[0]["name"] != $contextStack[1]["name"])
-              $this->data[$file . ":" . $tokens[$beginIndex][2] . ":" . $tokens[$beginIndex][3]]
-                = $contextStack[0]["name"];
+            array_unshift($contextStack, ['name' => $name, 'entered' => false, 'isClass' => false]);
+            if ($contextStack[0]['name'] != $contextStack[1]['name'])
+              $this->data[$file][$tokens[$beginIndex][2] . ':' . $tokens[$beginIndex][3]] = $contextStack[0]['name'];
             $beginIndex = 0;
             continue;
           }
           if ($tokens[$index][0] == T_CLASS) {
             if ($beginIndex == 0)
               $beginIndex = $index;
-            $name = "";
+            $name = '';
             $nextToken();
             while (isset($tokens[$index]) && in_array($tokens[$index][0], $nameTokens)) {
               $name .= trim($tokens[$index][1]);
               $nextToken();
             }
             array_unshift($contextStack, [
-              "name" => trim($contextStack[0]["name"] . "\\" . $name, "\\"),
-              "entered" => false,
-              "isClass" => true,
+              'name' => trim($contextStack[0]['name'] . '\\' . $name, '\\'),
+              'entered' => false,
+              'isClass' => true,
             ]);
-            if ($contextStack[0]["name"] != $contextStack[1]["name"])
-              $this->data[$file . ":" . $tokens[$beginIndex][2] . ":" . $tokens[$beginIndex][3]]
-                = $contextStack[0]["name"];
+            if ($contextStack[0]['name'] != $contextStack[1]['name'])
+              $this->data[$file][$tokens[$beginIndex][2] . ':' . $tokens[$beginIndex][3]] = $contextStack[0]['name'];
             $beginIndex = 0;
             continue;
           }
           if ($tokens[$index][0] == T_FUNCTION) {
             if ($beginIndex == 0)
               $beginIndex = $index;
-            $name = "";
+            $name = '';
             $nextToken();
             while (isset($tokens[$index]) && in_array($tokens[$index][0], $nameTokens)) {
               $name .= trim($tokens[$index][1]);
               $nextToken();
             }
             array_unshift($contextStack, [
-              "name" => trim($contextStack[0]["name"] . ($contextStack[0]["isClass"] ? "::" : "\\") . $name, "\\:"),
-              "entered" => false,
-              "isClass" => false,
+              'name' => trim($contextStack[0]['name'] . ($contextStack[0]['isClass'] ? '::' : '\\') . $name, '\\:'),
+              'entered' => false,
+              'isClass' => false,
             ]);
-            if ($contextStack[0]["name"] != $contextStack[1]["name"])
-              $this->data[$file . ":" . $tokens[$beginIndex][2] . ":" . $tokens[$beginIndex][3]]
-                = $contextStack[0]["name"];
+            if ($contextStack[0]['name'] != $contextStack[1]['name'])
+              $this->data[$file][$tokens[$beginIndex][2] . ':' . $tokens[$beginIndex][3]] = $contextStack[0]['name'];
             $beginIndex = 0;
             continue;
           }
@@ -100,25 +98,24 @@ class SymbolFinder {
           if (in_array($tokens[$index][0], [T_PUBLIC, T_PROTECTED, T_PRIVATE, T_ABSTRACT, T_DOC_COMMENT]))
             if ($beginIndex == 0)
               $beginIndex = $index;
-          if ($tokens[$index][1] == "{") {
-            if (!$contextStack[0]["entered"])
-              $contextStack[0]["entered"] = true;
+          if ($tokens[$index][1] == '{') {
+            if (!$contextStack[0]['entered'])
+              $contextStack[0]['entered'] = true;
             else
               array_unshift($contextStack, [
-                "name" => $contextStack[0]["name"],
-                "entered" => true,
-                "isClass" => false,
+                'name' => $contextStack[0]['name'],
+                'entered' => true,
+                'isClass' => false,
               ]);
             $nextToken();
             continue;
           }
-          if ($tokens[$index][1] == "}" && count($contextStack) > 2) {
-            if ($contextStack[0]["name"] != $contextStack[1]["name"])
-              $this->data[
-                $file
-                . ":" . ($tokens[$index][2] + count(explode("\n", $tokens[$index][1])) - 1)
-                . ":" . ($tokens[$index][3] + strlen($tokens[$index][1]))
-              ] = $contextStack[1]["name"];
+          if ($tokens[$index][1] == '}' && count($contextStack) > 2) {
+            if ($contextStack[0]['name'] != $contextStack[1]['name'])
+              $this->data[$file][
+                ($tokens[$index][2] + count(explode("\n", $tokens[$index][1])) - 1)
+                . ':' . ($tokens[$index][3] + strlen($tokens[$index][1]))
+              ] = $contextStack[1]['name'];
             array_shift($contextStack);
             $nextToken();
             continue;
@@ -126,21 +123,20 @@ class SymbolFinder {
           $nextToken();
         }
         while (count($contextStack) > 2) {
-          $this->data[
-            $file
-            . ":" . (end($tokens)[2] + count(explode("\n", end($tokens)[1])) - 1)
-            . ":" . (end($tokens)[3] + strlen(end($tokens)[1]))
-          ] = $contextStack[0]["name"];
+          $this->data[$file][
+            (end($tokens)[2] + count(explode("\n", end($tokens)[1])) - 1)
+            . ':' . (end($tokens)[3] + strlen(end($tokens)[1]))
+          ] = $contextStack[0]['name'];
           array_shift($contextStack);
         }
-        $this->data[$file . ":maxColumn"] = $maxColumn;
+        $this->data[$file]['maxColumn'] = $maxColumn;
       }
     }
     for ($scanLine = $line; $scanLine > 0; $scanLine -= 1)
-      for ($scanColumn = $this->data[$file . ":maxColumn"]; $scanColumn > 0; $scanColumn -= 1)
-        if (isset($this->data[$file . ":" . $scanLine . ":" . $scanColumn]))
-          return $this->data[$file . ":" . $scanLine . ":" . $scanColumn]
-            ? $this->data[$file . ":" . $scanLine . ":" . $scanColumn]
+      for ($scanColumn = $this->data[$file]['maxColumn']; $scanColumn > 0; $scanColumn -= 1)
+        if (isset($this->data[$file][$scanLine . ':' . $scanColumn]))
+          return $this->data[$file][$scanLine . ':' . $scanColumn]
+            ? $this->data[$file][$scanLine . ':' . $scanColumn]
             : ($fileSymbol ? $fileSymbol : $file);
     return $fileSymbol ? $fileSymbol : $file;
   }
