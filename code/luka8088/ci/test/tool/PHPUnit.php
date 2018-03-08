00a7b593e2e03fddcfb7d4b232b9bed81fd20e8c
luka8088 <luka8088@nrsk.net>
2018-03-08 22:40:26 +0000

    Use phpdbg

--- code/luka8088/ci/test/tool/PHPUnit.php
+++ code/luka8088/ci/test/tool/PHPUnit.php
@@ -4,11 +4,11 @@ namespace luka8088\ci\test\tool;
 
 use \Exception;
 use \luka8088\ci\Application;
+use \luka8088\ci\Internal;
 use \luka8088\ci\SymbolFinder;
 use \luka8088\ci\test\Result;
 use \luka8088\phops as op;
 use \SimpleXMLElement;
-use \Symfony\Component\Process\PhpExecutableFinder;
 use \Symfony\Component\Process\Process;
 
 class PHPUnit {
@@ -43,16 +43,36 @@ class PHPUnit {
     if (!$executable)
       throw new Exception('PHPUnit executable not found.');
 
+    $phpExecutablePath = PHP_BINARY ? PHP_BINARY : PHP_BINDIR . '/php';
+    $phpExecutableArguments = '';
+
+    if (is_file(dirname($phpExecutablePath) . '/phpdbg') || is_file(dirname($phpExecutablePath) . '/phpdbg.exe')) {
+
+      $alteredINIFile = tmpfile();
+      fwrite($alteredINIFile, Internal::disableINIXDebug(Internal::loadedINI()));
+      $alteredINIFileInfo = stream_get_meta_data($alteredINIFile);
+
+      $phpExecutablePath = dirname($phpExecutablePath) . '/phpdbg';
+      $phpExecutableArguments = ''
+        . ' ' . '-qrr'
+        . ' ' . '-c ' . (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN'
+          ? '"' . addcslashes($alteredINIFileInfo['uri'], '\\"') . '"'
+          : escapeshellarg($alteredINIFileInfo['uri']))
+      ;
+
+    }
+
+    if (!is_file($phpExecutablePath) && !is_file($phpExecutablePath . '.exe'))
+      throw new Exception('PHPUnit executable not found.');
+
     $testReportFile = tmpfile();
     $testReportFileInfo = stream_get_meta_data($testReportFile);
 
     $coverageReportFile = tmpfile();
     $coverageReportFileInfo = stream_get_meta_data($coverageReportFile);
 
-    $phpExecutableFinder = new PhpExecutableFinder();
-
     $process = new Process(
-      $phpExecutableFinder->find()
+      $phpExecutablePath . $phpExecutableArguments
       #. ' ' . '-dzend.enable_gc=0'
       . ' ' . escapeshellarg($executable)
       . ' ' . '--configuration ' . escapeshellarg($this->configuration)
@@ -126,12 +146,12 @@ class PHPUnit {
     if ($coverageReportSource) {
       $coverageReport = new SimpleXMLElement($coverageReportSource);
       $symbolCoverageMap = [];
-      foreach ($coverageReport->xpath(".//file") as $file)
-        foreach ($file->xpath(".//line[@type=\"stmt\"]") as $lineCoverage) {
-          $fileSymbol = ltrim(str_replace('\\', '/', substr(
-            realpath($file->attributes()->name->__toString()),
-            strlen(realpath(op\metaContext(Application::class)->getParameter('rootPath')))
-          )), '/');
+      foreach ($coverageReport->xpath('.//file') as $file) {
+        $fileSymbol = ltrim(str_replace('\\', '/', substr(
+          $file->attributes()->name->__toString(),
+          strlen(realpath(op\metaContext(Application::class)->getParameter('rootPath')))
+        )), '/');
+        foreach ($file->xpath('.//line[@type="stmt"]') as $lineCoverage) {
           $symbolLocation = $symbolFinder->findByLocation(
             $file->attributes()->name->__toString(),
             $lineCoverage->attributes()->num->__toString(),
@@ -142,6 +162,7 @@ class PHPUnit {
             $symbolCoverageMap[$symbolLocation] = [];
           $symbolCoverageMap[$symbolLocation][] = $lineCoverage->attributes()->count->__toString();
         }
+      }
 
       foreach ($symbolCoverageMap as $symbol => $symbolCoverage) {
         $coverage = count(array_filter($symbolCoverage)) / count($symbolCoverage);
