1abac3de2631a18aa095f52860e9c54def0dd576
Jesús Flores <me@jesusfloressanjose.com>
2019-01-10 18:35:19 +0000

    Update ci.configuration.distributed.php

--- ci.configuration.distributed.php
+++ ci.configuration.distributed.php
@@ -4,7 +4,7 @@ return function ($ci) {
 
     $ci[] = new \luka8088\ci\test\tool\InlineTest();
 
-    $ci->addPath(__dir__ . '/code');
-    $ci->addPath(__dir__ . '/test');
+    $ci->addPath(__dir__ . '/src');
+    $ci->addPath(__dir__ . '/tests');
 
 };
